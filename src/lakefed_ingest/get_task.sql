1be4f0fb359578fd98aae143bfb16029e21480c7
Chris Koester <123766685+chris-koester-db@users.noreply.github.com>
2025-06-30 09:49:56 -0400

    Organize and update jobs (#37)

--- src/lakefed_ingest/get_task.sql
+++ src/lakefed_ingest/get_task.sql
@@ -1,5 +1,7 @@
 -- Get task metadata
 -- Workflow dynamic value references for SQL output are limited to 1,000 rows and 48 KB
-select * from identifier(:ctrl_catalog || '.' || :ctrl_schema || '.' || :ctrl_table)
+-- Convert array to comma separated string for use with workflow dynamic value references
+select *
+from identifier(:ctrl_catalog || '.' || :ctrl_schema || '.' || :ctrl_table)
 where id = :task_id
 |> set sink_cluster_cols = case when array_size(sink_cluster_cols) > 0 then array_join(sink_cluster_cols, ', ') else null end
--- src/lakefed_ingest/get_task_ids.sql
+++ src/lakefed_ingest/get_task_ids.sql
@@ -1,5 +1,8 @@
 -- Get task ids for specified job and task collection
 -- Workflow dynamic value references for SQL output are limited to 1,000 rows and 48 KB
-select id from identifier(:ctrl_catalog || '.' || :ctrl_schema || '.' || :ctrl_table)
+-- Convert array to comma separated string for use with workflow dynamic value references
+select id
+from identifier(:ctrl_catalog || '.' || :ctrl_schema || '.' || :ctrl_table)
 where task_collection = :task_collection
+and load_partitioned = :load_partitioned
 and task_enabled is true
